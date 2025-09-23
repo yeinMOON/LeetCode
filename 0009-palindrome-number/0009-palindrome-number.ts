b93c8263f6cc798a0f95397b190b3e8353ef4e5b
Yein Moon <85829962+yeinMOON@users.noreply.github.com>
2025-09-23 22:59:03 +0900

    Time: 14 ms (15.39%), Space: 65.3 MB (57.37%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.ts
+++ 0009-palindrome-number/0009-palindrome-number.ts
@@ -1,13 +1,4 @@
 function isPalindrome(x: number): boolean {
-    // from minus to 99
-    if (x < 0) return false;
-    if (x >= 0 && x <= 9) return true;
-    if (x >= 10 && x <= 19) return x === 11;
-
-    const array = x.toString().split('');
-    const middle = Math.ceil((array.length - 1) / 2);
-    const head = array.slice(0, middle);
-    const tail = array.slice(middle + (x % 2 ? 0 : -1)).reverse();
-
-    return head.some((num, index) => num !== tail[index]) ? false : true
+    const reversed = x.toString().split('').reverse().join('');
+    return x.toString() === reversed
 };
