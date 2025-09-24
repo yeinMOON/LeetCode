8941a774f8772466168daaf4dfb226c6c0a5ef00
Yein Moon <85829962+yeinMOON@users.noreply.github.com>
2025-09-24 23:47:22 +0900

    Time: 13 ms (27.84%), Space: 58.6 MB (36.16%) - LeetHub

--- 0268-missing-number/0268-missing-number.ts
+++ 0268-missing-number/0268-missing-number.ts
@@ -1,7 +1,15 @@
 function missingNumber(nums: number[]): number {
     const sorted = nums.toSorted((a, b) => a - b);
-    const breakpoint = sorted.findIndex((item, index) => item !== index)
+    const breakpoint = sorted.findIndex((item, index) => item !== index);
 
     if (breakpoint >= 0) return breakpoint;
     return sorted[0] === 0 ? nums.length : 0;
 };
+
+// 천잰가
+// function missingNumber(nums: number[]): number {
+//     const n = nums.length;
+//     const expected = (n * (n + 1)/2);
+//     const actual = nums.reduce((a, b) => a + b, 0);
+//     return expected - actual;
+// };
